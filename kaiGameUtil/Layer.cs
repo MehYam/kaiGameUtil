a306f30be76669232fdaa641e3237e0ecc72e642
Kai Arnold <cdn.kai@gmail.com>
2017-02-19 15:46:09 -0800

    first

---
+++ kaiGameUtil/Layer.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace kaiGameUtil
+{
+    public sealed class Layer<T>
+    {
+        readonly T[,] tiles;
+
+        public readonly Point<int> size;
+        public Layer(int width, int height)
+        {
+            tiles = new T[width, height];
+            this.size = new Point<int>(width, height);
+        }
+        public Layer(Layer<T> rhs)
+        {
+            tiles = new T[rhs.size.x, rhs.size.y];
+            this.size = new Point<int>(rhs.size.x, rhs.size.y);
+
+            ForEach((x, y, tile) =>
+            {
+                //KAI: latent bug - this is a shallow copy, which happens to work now because T is always a value type (i.e. struct Tile)
+                tiles[x, y] = rhs.Get(x, y);
+            });
+        }
+        public T Get(int x, int y)
+        {
+            return tiles[x, y];
+        }
+        public void Set(int x, int y, T t)
+        {
+            tiles[x, y] = t;
+        }
+        public T Get(Point<int> pos)
+        {
+            return tiles[pos.x, pos.y];
+        }
+        public void Set(Point<int> pos, T t)
+        {
+            tiles[pos.x, pos.y] = t;
+        }
+        /// <summary>
+        ///  Iterate the layer, invoking the callback with (x, y, item) arguments
+        /// </summary>
+        /// <param name="callback"></param>
+        public void ForEach(Action<int, int, T> callback)
+        {
+            for (var y = 0; y < size.y; ++y)
+            {
+                for (var x = 0; x < size.x; ++x)
+                {
+                    callback(x, y, tiles[x, y]);
+                }
+            }
+        }
+        /// <summary>
+        /// Iterate every other tile in the layer, invoking the callback with (x, y, item) arguments
+        /// </summary>
+        /// <param name="callback"></param>
+        public void ForEveryOther(Action<int, int, T> callback)
+        {
+            for (var y = 0; y < size.y; ++y)
+            {
+                for (var x = y % 2; x < size.x; x += 2)
+                {
+                    callback(x, y, tiles[x, y]);
+                }
+            }
+        }
+        public void Fill(Func<int, int, T, T> callback)
+        {
+            for (var y = 0; y < size.y; ++y)
+            {
+                for (var x = 0; x < size.x; ++x)
+                {
+                    tiles[x, y] = callback(x, y, tiles[x, y]);
+                }
+            }
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            int thisY = -1;
+            ForEach((int x, int y, T t) => 
+            {
+                if (thisY != y)
+                {
+                    thisY = y;
+                    if (thisY > 0)
+                    {
+                        sb.AppendLine();
+                    }
+                    sb.Append((thisY % 10) + " ");
+                }
+                sb.Append(t.ToString());
+            });
+            return sb.ToString();
+        }
+    }
+}
---
+++ test/Test.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using kaiGameUtil;
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        TestPoint();
+        TestLayer();
+
+        Console.ReadKey(false);
+    }
+    static void TestPoint()
+    {
+        var a = new Point<int>(0, 0);
+        var b = new Point<int>(2, 2);
+
+        var c = new Point<float>(2.5f, 3.5f);
+        var d = new Point<float>(1, 2);
+
+        Console.WriteLine("IEquatable (should be true): " + (new Point<int>(2, 2) == b));
+        Console.WriteLine("IEquatable (should be false): " + (a == b));
+        Console.WriteLine("Copy (should be true): " + (new Point<int>(b) == b));
+        Console.WriteLine("Addition ({0}) ({1})", Util.Add(a, b), Util.Add(c, d));
+        Console.WriteLine("Addition ({0}) ({1})", Util.Add(a, 3), Util.Add(c, 3.5f));
+        Console.WriteLine("Subtraction ({0}) ({1})", Util.Subtract(a, b), Util.Subtract(c, d));
+        Console.WriteLine("Subtraction ({0}) ({1})", Util.Subtract(a, 3), Util.Subtract(c, 3.5f));
+        Console.WriteLine("Multiplication ({0}) ({1})", Util.Multiply(a, b), Util.Multiply(c, d));
+        Console.WriteLine("Multiplication ({0}) ({1})", Util.Multiply(b, 3), Util.Multiply(c, 3));
+        Console.WriteLine("Division ({0}) ({1})", Util.Divide(b, b), Util.Divide(c, d));
+        Console.WriteLine("Division ({0}) ({1})", Util.Divide(b, 2), Util.Divide(c, 10));
+    }
+    struct TestTile
+    {
+        public static char[] types = { ' ', '.', 'o', 'O', '8', '#' };
+        public static bool IsPassable(char type) { return type == ' ' || type == '.'; }
+
+        public readonly int layer;
+        public readonly Point<int> pos;
+        public readonly char type;
+        public TestTile(char type)
+        {
+            this.layer = 0;
+            this.pos = new Point<int>(0, 0);
+            this.type = type;
+        }
+        public TestTile(int layer, int x, int y, char type)
+        {
+            this.layer = layer;
+            this.pos = new Point<int>(x, y);
+            this.type = type;
+        }
+        public override string ToString()
+        {
+            return type.ToString();
+        }
+        public string ToStringLong()
+        {
+            return string.Format("L:{0}, {1} T:{2:00} |", layer, pos, type);
+        }
+    }
+    static void TestLayer()
+    {
+        var noise = new Perlin();
+        var layer = new Layer<TestTile>(120,30);
+        const float zoom = 3;
+        layer.Fill((x, y, oldTile) =>
+        {
+            double perlin = noise.perlin((float)x * zoom/layer.size.x, (float)y * zoom/layer.size.y, 0.3);
+
+            char tile = TestTile.types[(int)(perlin * TestTile.types.Length)];
+            return new TestTile(0, x, y, tile);
+        });
+
+        Console.WriteLine(layer);
+    }
+}
+
