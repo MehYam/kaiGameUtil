977ce822f6e0ee2bc97dd7b97c8c49854b6f30c1
Kai Arnold <cdn.kai@gmail.com>
2017-02-22 17:26:41 -0800

    more consolidation

--- kaiGameUtil/Layer.cs
+++ kaiGameUtil/Layer.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace kaiGameUtil
@@ -42,13 +40,17 @@ namespace kaiGameUtil
         {
             tiles[pos.x, pos.y] = t;
         }
+        public bool IsValid(Point<int> pos)
+        {
+            return pos.x >= 0 && pos.y >= 0 && pos.x < size.x && pos.y < size.y;
+        }
         /// <summary>
         ///  Iterate the layer, invoking the callback with (x, y, item) arguments
         /// </summary>
         /// <param name="callback"></param>
         public void ForEach(Action<int, int, T> callback)
         {
-            for (var y = 0; y < size.y; ++y)
+            for (var y = size.y - 1; y >= 0; --y)
             {
                 for (var x = 0; x < size.x; ++x)
                 {
@@ -93,20 +95,15 @@ namespace kaiGameUtil
         public override string ToString()
         {
             var sb = new StringBuilder();
-            int thisY = -1;
-            ForEach((int x, int y, T t) => 
+            int currentY = size.y - 1;
+            ForEach((int x, int y, T t) =>
             {
-                if (thisY != y)
+                if (currentY != y)
                 {
-                    thisY = y;
-                    if (thisY > 0)
-                    {
-                        sb.AppendLine();
-                    }
-                    sb.Append((thisY % 10) + " ");
+                    currentY = y;
+                    sb.AppendLine();
                 }
-                if (t != null) sb.Append(t.ToString());
-                sb.Append(" ");
+                sb.Append(t.ToString());
             });
             return sb.ToString();
         }
