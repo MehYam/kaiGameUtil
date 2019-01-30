029b41f0b7d9dfbb435f556ad8d1dfb20ca7e39d
Kai Arnold <cdn.kai@gmail.com>
2019-01-30 10:57:35 -0800

    add FileUtil + unit tests, set target to .NET Standard 2.0

---
+++ UnitTest/FileUtilTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using kaiGameUtil;
+namespace UnitTest
+{
+    [TestClass]
+    public class FileUtilTest
+    {
+        private bool TestSplitResult(string toSplit, string[] expected)
+        {
+            var result = FileUtil.SplitStringIntoLines(toSplit);
+            return Enumerable.SequenceEqual(result, expected);
+        }
+        [TestMethod]
+        public void SplitStringShouldSplit()
+        {
+            Assert.IsTrue(TestSplitResult("", new string[] { "" }));
+            Assert.IsTrue(TestSplitResult("\r", new string[] { "\r" }));
+            Assert.IsTrue(TestSplitResult("\n", new string[] { "", "" }));
+            Assert.IsTrue(TestSplitResult("\r\n", new string[] { "", "" }));
+            Assert.IsTrue(TestSplitResult("foo\nbar", new string[] { "foo", "bar" }));
+        }
+        private bool TestSplitAndTrimResult(string toSplit, char delim, string[] expected)
+        {
+            var result = FileUtil.SplitAndTrim(toSplit, delim);
+            return Enumerable.SequenceEqual(result, expected);
+        }
+        [TestMethod]
+        public void SplitAndTrimShouldSplitAndTrim()
+        {
+            Assert.IsTrue(TestSplitAndTrimResult("", ',', new string[] { "" }));
+            Assert.IsTrue(TestSplitAndTrimResult("foo,bar", ',', new string[] { "foo", "bar" }));
+            Assert.IsTrue(TestSplitAndTrimResult(" foo , bar ", ',', new string[] { "foo", "bar" }));
+        }
+    }
+}
---
+++ kaiGameUtil/FileUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace kaiGameUtil
+{
+    public static class FileUtil
+    {
+        public static string ReadFileIntoString(string fileName)
+        {
+            return File.ReadAllText(fileName);
+        }
+        private static readonly string[] lineSplitSpec = new string[] { "\r\n", "\n" };
+        public static string[] SplitStringIntoLines(string str)
+        {
+            return str.Split(lineSplitSpec, System.StringSplitOptions.None);
+        }
+        public static string[] SplitAndTrim(string str, char delimiter)
+        {
+            var strs = str.Split(new char[] { delimiter });
+            var trimmed = Array.ConvertAll<string, string>(strs, s => s.Trim());
+            return trimmed;
+        }
+    }
+}
